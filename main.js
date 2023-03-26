f2b79d7224f1027c7e09dc520d1d56deafd0e80f
sanaacoder05 <96862268+sanaacoder05@users.noreply.github.com>
2023-03-26 10:46:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,62 @@
+img="";
+
+function preload(){
+    img = loadImage("f.jpeg");
+}
+
+function setup(){
+    canvas = createCanvas(650,450);
+    canvas.center();
+    objectDetection = ml5.objectDetector('cocossd',Modelloaded);
+    document.getElementById('status').innerHTML="status :  detecting objects";
+}
+function Modelloaded(){
+   console.log('model loaded**');
+   checkbox = true;
+
+   objectDetection.detect(img,gotresults);
+   
+}
+
+function gotresults(error,result){
+   if(error){
+       console.log(error);
+
+   }
+
+   else{
+       console.log(result);
+   }
+}
+function draw(){
+    image(img,0,0,650,450);
+    fill("#ffffff");
+    text("nasty dishes",260,368);
+    noFill();
+    stroke("black");
+    rect(260,368,90,70);
+
+    fill("#ffffff");
+    text("lost remote",500,180);
+    noFill();
+    stroke("black");
+    rect(500,180,130,90);
+
+    fill("#ffffff");
+    text("mom's lost hangbag",170,168);
+    noFill();
+    stroke("black");
+    rect(170,168,85,60);
+
+    fill("#ffffff");
+    text("laptop which has faced a lot",330,90);
+    noFill();
+    stroke("black");
+    rect(330,90,85,60);
+
+    fill("#ffffff");
+    text("soup bowl ,which is not washed for days",375,145);
+    noFill();
+    stroke("black");
+    rect(372,139,50,50);
+}
